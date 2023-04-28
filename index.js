0a9bd6e8f5c0ea14ad47f3be3ed56c200265a08a
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-04-28 16:53:55 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+import { useCallback, useMemo } from "react";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  query,
+  setDoc,
+} from "firebase/firestore";
+import { useCollection as rfhUseCollection } from "react-firebase-hooks/firestore";
+
+export default function useCollection(db, dbName, { queries = [] } = {}) {
+  const collectionRef = useMemo(() => collection(db, dbName), [db, dbName]);
+  const [collectionData, loading, error] = rfhUseCollection(
+    query(collectionRef, ...queries)
+  );
+
+  const docs = useMemo(() => {
+    return collectionData?.docs?.map((doc) => ({ ...doc.data(), id: doc.id }));
+  }, [collectionData]);
+
+  const docsById = useMemo(() => {
+    return (
+      collectionData?.docs?.reduce(
+        (acc, doc) => ({
+          ...acc,
+          [doc.id]: { ...doc.data(), id: doc.id },
+        }),
+        {}
+      ) || {}
+    );
+  }, [collectionData]);
+
+  const count = useMemo(() => {
+    return collectionData?.docs?.length || 0;
+  }, [collectionData]);
+
+  const addEntry = useCallback(
+    async (card) => {
+      const addRes = await addDoc(collectionRef, card);
+      const getRes = await getDoc(doc(collectionRef, addRes.id));
+      return { id: addRes.id, ...getRes.data() };
+    },
+    [collectionRef]
+  );
+
+  const modifyEntry = useCallback(
+    async ({ id, ...card }) => {
+      await setDoc(doc(collectionRef, id), card);
+      const getRes = await getDoc(doc(collectionRef, id));
+      return { id, ...getRes.data() };
+    },
+    [collectionRef]
+  );
+
+  const deleteEntry = useCallback(
+    async (id) => {
+      await deleteDoc(doc(collectionRef, id));
+    },
+    [collectionRef]
+  );
+
+  const status = useMemo(
+    () => (loading ? "loading" : error ? "error" : "loaded"),
+    [loading, error]
+  );
+
+  return {
+    allList: docs,
+    allById: docsById,
+    count,
+    status,
+    add: addEntry,
+    modify: modifyEntry,
+    remove: deleteEntry,
+  };
+}
